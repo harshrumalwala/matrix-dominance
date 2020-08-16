455960b3e9e299b9734cae8a9b73b8ebd2cc8fc3
Harsh Rumalwala <harsh.ashvin@gmail.com>
2020-08-17 01:29:58 +0530

    initial rough design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import _ from 'lodash';
+
+import { Container, Row, Dot, Cell } from 'styles';
+import LineTo from 'react-lineto';
 
 const App = () => {
+  const [edges, setEdges] = useState({});
+  const [matrix, setMatrix] = useState([]);
+  const [start, setStart] = useState(-1);
+  const matrixSize = 4;
+
+  const mergeUtil = (
+    a: { [key: string]: Array<number> },
+    b: { [key: string]: Array<number> }
+  ) => {
+    let result: { [key: string]: Array<number> } = a;
+    _.forEach(b, (v: Array<number>, k: string) => {
+      result[k] = _.has(result, k) ? _.union(result[k], v) : v;
+    });
+    return result;
+  };
+
+  const handleClick = (e: React.MouseEvent<HTMLInputElement>, idx: number) => {
+    e.preventDefault();
+
+    if (start === -1) setStart(idx);
+    else {
+      setEdges((prevState) =>
+        mergeUtil(prevState, { [start]: [idx], [idx]: [start] })
+      );
+      setStart(-1);
+    }
+    console.log(edges);
+  };
+
   return (
-    <div className="App">
-      App
-    </div>
+    <Container>
+      {_.times(2 * matrixSize - 1, (i) => (
+        <Row key={i}>
+          {i % 2 === 0 &&
+            _.times(matrixSize, (j) => (
+              <Dot
+                key={i * matrixSize + j}
+                onClick={(e: React.MouseEvent<HTMLInputElement>) =>
+                  handleClick(e, i * matrixSize + j)
+                }
+                className={`dot_${i * matrixSize + j}`}
+              />
+            ))}
+          {i % 2 === 1 &&
+            _.times(matrixSize - 1, (k) =>
+              k === 0 ? (
+                <Cell key={i * matrixSize + k} className="left-cell">
+                  HR
+                </Cell>
+              ) : (
+                <Cell key={i * matrixSize + k}>HR</Cell>
+              )
+            )}
+        </Row>
+      ))}
+      {_.map(edges, (v, k) =>
+        _.map(v, (o) => <LineTo from={`dot_${o}`} to={`dot_${k}`} delay={0} />)
+      )}
+    </Container>
   );
-}
+};
 
 export default App;
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,7 +1,28 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
-export const Row = styled.div``;
+export const Row = styled.div`
+  display: flex;
+  margin-left: 10px;
+  font-size: 0px;
+`;
 
-export const Block = styled.div``;
+export const Dot = styled.span`
+  margin: 10px 0px 0px 30px;
+  cursor: pointer;
+  height: 13px;
+  width: 13px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+`;
+
+export const Cell = styled.div`
+  // z-index: 100;
+  margin: 10px 0px 0px 30px;
+  font-size: 10px;
+`;
