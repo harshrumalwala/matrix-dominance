ba24d96c60dcd3f5e2a8792b356786f24679f292
Harsh Rumalwala <harsh.ashvin@gmail.com>
2020-09-14 12:28:16 +0530

    correcting a condition

--- src/hooks/use-create-new-game/index.ts
+++ src/hooks/use-create-new-game/index.ts
@@ -16,7 +16,7 @@ const useCreateNewGame = (isExisting: boolean): CreateNewGameOutput => {
   ) => {
     setIsCreatingNewGame(true);
     try {
-      isExisting
+      return isExisting
         ? await db
             .collection('rooms')
             .doc(roomId)
@@ -25,10 +25,10 @@ const useCreateNewGame = (isExisting: boolean): CreateNewGameOutput => {
             .collection('rooms')
             .add(getNewGameState(players, host, matrixSize))
             .then((doc) => doc.id);
-      setIsCreatingNewGame(false);
     } catch (error) {
       console.error('Error while updating the board');
     }
+    setIsCreatingNewGame(false);
   };
 
   return { isCreatingNewGame, createNewGame };
